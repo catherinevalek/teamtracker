7eb855113205258214c31568f387dc4e842daa52
Catherine Valek <catherine.valek@gmail.com>
2018-01-22 15:04:12 -0600

    add team and position views and controllers

---
+++ app/controllers/positions_controller.rb
@@ -0,0 +1,55 @@
+class PositionsController < ApplicationController
+
+  def index
+    @positions = Position.all
+  end
+
+  def new
+    @position = Position.new
+  end
+
+  def create
+    @position = Position.new(position_params)
+
+    if @position.save
+      flash[:success] = "Position created!"
+      redirect_to @position
+    else
+      render :new
+    end
+  end
+
+  def show
+    @position = Position.find(params[:id])
+  end
+
+  def edit
+    @position = Position.find(params[:id])
+  end
+
+  def update
+    @position = Position.find(params[:id])
+    
+    if @position.update_attributes(position_params)
+      flash[:success] = "Your changes have been updated!"
+      redirect_to @position
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Position.find(params[:id]).destroy
+    flash[:success] = "Position deleted."
+    redirect_to positions_url
+  end
+
+  private
+
+  def position_params
+    params.require(:position).permit(:name, :description, :code)
+  end
+
+end
+
+
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,1 +1,55 @@
-teams_controller.rb
+class TeamsController < ApplicationController
+
+  def index
+    @teams = Team.all
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      flash[:success] = "team created!"
+      redirect_to @team
+    else
+      render :new
+    end
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    
+    if @team.update_attributes(team_params)
+      flash[:success] = "Your changes have been updated!"
+      redirect_to @team
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Team.find(params[:id]).destroy
+    flash[:success] = "team deleted."
+    redirect_to teams_url
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:city, :nickname, :colors, :email, :logo)
+  end
+
+end
+
+
