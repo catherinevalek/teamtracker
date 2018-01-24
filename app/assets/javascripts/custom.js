7748bbe857196ccf7b277aa42ecec55224a56f87
Catherine Valek <catherine.valek@gmail.com>
2018-01-24 14:31:59 -0600

    add excel download for the most part

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,48 @@
+// $(document).ready(function() {
+//     $('.index-table').DataTable( {
+//     "lengthMenu": [[5, 10, -1], [5, 10, "All"]],
+//     "pageLength": -1,
+//     dom: 'Bfrtip',
+//     buttons: [
+//         {
+//             extend: 'excel',
+//             text: 'Save current page',
+//             exportOptions: {
+//                 modifier: {
+//                     page: 'current'
+//                 }
+//             }
+//         }
+//     ]
+//     } );
+// });
+
+
 $(document).ready(function() {
-    $('#team-table').DataTable( {
+    var table = $('.index-table').DataTable( {
     "lengthMenu": [[5, 10, -1], [5, 10, "All"]],
-    "pageLength": -1
+    "pageLength": -1,
+    dom: 'Bfltip',
+    buttons: [
+        {
+            extend: 'excel',
+            text: 'Save current page',
+            exportOptions: {
+                modifier: {
+                    page: 'current'
+                }
+            }
+        }
+    ],
+
+        "columnDefs": [
+          { "orderable": false, "targets": [-2, 2] },
+          // { "searchable": false, "targets": [-1, 1] }
+        ]
+
+
     } );
-});
+ 
+    table.buttons().container()
+        .insertBefore( '.index-table_filter' );
+} );
